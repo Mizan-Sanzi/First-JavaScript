4df48b895abd129ad9f28a6c31e532b1220a5f0b
mizansanzi <pleasemailmizan@gmail.com>
2024-08-25 21:33:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+alert("This is first javascript file's alert!");
+document.write("<h1>This is new text</h1>");
+let a_story_book; // variable declaration
+a_story_book = "book_one"; //defining variable
+alert(a_story_book); //using variable
